8bce15c0cd8a0b33b2bf2de0659c51d280644e61
raysulkobir <raysulkobir97@gmail.com>
2022-07-30 18:26:37 +0600

    Initial commit

---
+++ measuring-time.js
@@ -0,0 +1,20 @@
+// Example 1
+
+console.time('response in');
+console.log('Click to continue');
+console.timeEnd('response in');
+console.log('One more time');
+console.timeEnd('response in')
+
+
+
+// Example 2
+var start = new Date().getTime();
+
+for (i = 0; i < 50000; ++i) {
+// do something
+}
+
+var end = new Date().getTime();
+var time = end - start;
+console.log('Execution time: ' + time);
